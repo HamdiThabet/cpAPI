85660f80f2bf7b68c87b683cd1b8a3c2622384b3
HamdiThabet <hhaammddoouucchh@gmail.com>
2021-09-22 13:38:59 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,33 @@
 import logo from './logo.svg';
 import './App.css';
+import UserList from './UserList';
+import { useEffect, useState } from 'react';
+import axios from 'axios'
+import faker from 'faker'
 
 function App() {
+  const [list, setlist] = useState(null)
+  const [error, setError] = useState(null)
+  useEffect(() => {
+    axios.get("https://jsonplaceholder.typicode.com/users")
+      .then(response => setlist(response.data))
+      .catch(err => setError(err))
+  }, [])
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1> listOfUSer </h1>
+        {list && list.map(user => <div >
+          <img className=" imgss" src={faker.image.avatar()}></img>
+          {user.name}
+          <br />
+          {user.phone}
+          <br />
+          {user.email}
+          <br />
+          {user.address.city}
+
+        </div>)}
       </header>
     </div>
   );
---
+++ src/UserList.js
@@ -0,0 +1,14 @@
+import React, { useEffect } from 'react'
+
+const UserList = () => {
+    useEffect(() => {
+
+    }, [])
+    return (
+        <div>
+            <h1>user list</h1>
+        </div>
+    )
+}
+
+export default UserList
